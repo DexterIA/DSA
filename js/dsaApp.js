43267d5731ffacbee229694fc3e7f20f870d32cf
DexterIA <dexterrrrr@list.ru>
2016-01-27 22:47:44 +0300

    initial commit

---
+++ js/dsaApp.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('myApp', []);
+
+app.controller('userCtrl', function ($scope) {
+    $scope.message = 'sdfgsdfgsdfgsdfg';
+    $scope.publicKey = 'Test public key\nTest public key\nTest public key\nTest public key';
+    $scope.privateKey = 'Test private key\nTest private key\nTest private key\nTest private key';
+
+    $scope.generateKeys = function () {
+
+    };
+});
